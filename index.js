d5df660b895553f95046164ead04f85cb6da4f63
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-27 03:43:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
